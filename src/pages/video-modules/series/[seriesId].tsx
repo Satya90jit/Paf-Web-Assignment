829854bbcce7cbfe7be5645c5e7f6a3356d82f15
Satya90jit <satyajitsahu2002@gmail.com>
2024-06-29 04:09:05 +0530

    implement ISR

--- src/pages/video-modules/series/[seriesId].tsx
+++ src/pages/video-modules/series/[seriesId].tsx
@@ -5,24 +5,34 @@ import {
 } from "@/components/video-series";
 import { useSWRAPI } from "@/hooks";
 import { PublicLayout } from "@/layouts";
+import { constructImageUrl } from "@/utils";
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
 import { useRouter } from "next/router";
 
-const VideoSeriesPage = () => {
+const VideoSeriesPage = ({
+  data,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   const router = useRouter();
-  const { data, isValidating } = useSWRAPI(
+  const { data: seriesData, isValidating } = useSWRAPI(
     `https://api.acharyaprashant.org/v2/legacy/courses/series/optuser/${router?.query?.seriesId}`
   );
+  // console.log("title------->", data?.details?.title);
+
+  // console.log(".......data from getsattic0--->", data);
+  const imageUrl = constructImageUrl(data?.details?.thumbnail);
   return (
     <PublicLayout
-      title={`${data?.data?.details?.title || "वेदांत"} | आचार्य प्रशांत`}
+      title={`${data?.details?.title || "वेदांत"} | आचार्य प्रशांत`}
+      description={`${data?.details?.title}`}
+      ogImage={imageUrl}
     >
       <section className="w-full main-container pb-12 md:space-y-12 space-y-8">
         <VideoSeriesCard
-          courseDetails={data?.data?.details}
-          courses={data?.data?.courses}
+          courseDetails={data?.details}
+          courses={data?.courses}
           isValidating={isValidating}
         />
-        <RelatedContent relatedContents={data?.data?.relatedContent} />
+        <RelatedContent relatedContents={data?.relatedContent} />
         <FaqSection />
       </section>
     </PublicLayout>
@@ -30,3 +40,36 @@ const VideoSeriesPage = () => {
 };
 
 export default VideoSeriesPage;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const res = await fetch(
+    `https://api.acharyaprashant.org/v2/legacy/courses/series/optuser/course-series-eeb9d3`
+  );
+  const data = await res.json();
+  const series = data?.relatedContent as any[];
+
+  console.log("data-----?", series);
+
+  return {
+    paths: series?.map((blog) => ({ params: { seriesId: blog?.id } })),
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps<{ data: any }> = async (
+  context: any
+) => {
+  const seriesId = context?.params?.seriesId;
+
+  const res = await fetch(
+    `https://api.acharyaprashant.org/v2/legacy/courses/series/optuser/${seriesId}`
+  );
+  const courseData = await res.json();
+
+  return {
+    props: {
+      data: courseData,
+    },
+    // revalidate: 8,
+  };
+};
